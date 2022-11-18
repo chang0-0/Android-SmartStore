5f9d78150e4a1836bf33b81f95e0e023aaa8eb1c
bigyoung8375 <bigyoung8375@naver.com>
2022-11-18 11:28:48 +0900

    2022-11-18 11:28 Commit

--- client/smartstore_project_jetpack/app/src/main/java/com/ssafy/smartstore/viewModels/UserViewModel.kt
+++ client/smartstore_project_jetpack/app/src/main/java/com/ssafy/smartstore/viewModels/UserViewModel.kt
@@ -5,9 +5,6 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.ssafy.smartstore.dto.User
-import com.ssafy.smartstore.repository.UserRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 class UserViewModel(user: User, app: Application) : AndroidViewModel(app) {
     private val _userInfo = MutableLiveData<User>()
