6a8051bea02f6c46c4c7658b9851bfa815206b7a
Shin Hyunwoo <sisn0596@gmail.com>
2022-11-24 09:01:17 +0900

    test

--- client/smartstore_project_jetpack/app/src/main/java/com/ssafy/smartstore/activity/MainActivity.kt
+++ client/smartstore_project_jetpack/app/src/main/java/com/ssafy/smartstore/activity/MainActivity.kt
@@ -69,7 +69,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
