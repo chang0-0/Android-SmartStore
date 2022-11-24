2c4e22f7749b7aac86cd7b94a46fde7c57ddfb2e
bigyoung8375 <bigyoung8375@naver.com>
2022-11-24 09:49:18 +0900

    2022-11-24 09:48 server change

--- client/smartstore_project_jetpack/app/src/main/java/com/ssafy/smartstore/config/ApplicationClass.kt
+++ client/smartstore_project_jetpack/app/src/main/java/com/ssafy/smartstore/config/ApplicationClass.kt
@@ -20,7 +20,7 @@ class ApplicationClass : Application() {
         // ipconfig를 통해 ip확인하기
         // 핸드폰으로 접속은 같은 인터넷으로 연결 되어있어야함 (유,무선)
         //const val SERVER_URL = "http://mobile-pjt.sample.ssafy.io/"
-        const val SERVER_URL = "http://192.168.33.122:9999/"
+        const val SERVER_URL = "http://192.168.33.121:9999/"
         const val MENU_IMGS_URL = "${SERVER_URL}imgs/menu/"
         const val GRADE_IMGS_URL = "${SERVER_URL}imgs/grade/"
 
